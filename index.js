d50826b2bd82669fd828c4b50a97948e8df179ad
AnastasyKip <1746237@gmail.com>
2025-05-29 15:29:03 +0100

    Apply Prettier formatting

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-console.log('Hello, Hexlet!')
-import _ from 'lodash'
-let a = [1, 4, 5, 6, 3, 2, 7, 8, 9]
-let b = [4, 5, 5, 5, 6, 1, 7]
-console.log(_.intersection(a, b));
-console.log(_.last(['one', 'two']))
+let x = [1, 2, 3];
+console.log(x);
